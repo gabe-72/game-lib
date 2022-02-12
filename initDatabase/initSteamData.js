ce9957291cc28b7328c0cae9d83e3cbe03d5ed57
Gabriel Chung <gabrielchung9@gmail.com>
2022-02-12 18:31:11 -0500

    made initSteamData.js standalone

--- initDatabase/initSteamData.js
+++ initDatabase/initSteamData.js
@@ -1,14 +1,13 @@
 import fetch from "node-fetch";
+import * as fs from "fs";
 
 const SAMPLE_AMT = 10000;
 
-fetch("http://localhost:3000/steamAppDetails.json")
-  .then(res => res.json())
-  .then(res => res.applist.apps.map((data) => data.appid))
-  .then(res => {
-    for (let i = 0; i < SAMPLE_AMT; ++i) {
-      fetch("https://store.steampowered.com/api/appdetails?appids="+res[i])
-        .then(res => res.json())
-        .then(res => console.log(res));
-    }
-  });
+const raw_appids = JSON.parse(fs.readFileSync("./initDatabase/assets/steamAppDetails.json"));
+const appids = raw_appids.applist.apps.map(data => data.appid);
+
+for (let i = 0; i < SAMPLE_AMT; ++i) {
+  fetch("https://store.steampowered.com/api/appdetails?appids="+appids[i])
+    .then(res => res.json())
+    .then(res => console.log(res));
+}
