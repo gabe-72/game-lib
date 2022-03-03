cf9ec6a56c61da73c6cadb78b13e8e69ffdaa4af
Gabriel Chung <gabrielchung9@gmail.com>
2022-03-03 00:49:27 -0500

    refractored fetch requests into an async func

--- initDatabase/initSteamData.js
+++ initDatabase/initSteamData.js
@@ -1,32 +1,50 @@
 import fetch from "node-fetch";
 import * as fs from "fs";
 import sqlite3 from "sqlite3";
-let db = new sqlite3.Database("./game_lib.db", sqlite3.OPEN_CREATE, (err) => {
+
+const SAMPLE_AMT = 15;
+const APPIDS_PATH = "./initDatabase/assets/steamAppDetails.json";
+const REQ_URL = "https://store.steampowered.com/api/appdetails?appids=";
+
+// get the appdetails
+const appdetails = getAppDetails();
+
+// connect to the database
+let db = new sqlite3.Database("./game_lib.db", (err) => {
   if (err) return console.error(err.message);
-  console.log("Successfully connected to database");
+  console.log("Connected to database");
 });
 
-// how many apps to get
-const SAMPLE_AMT = 100;
+// TODO: insert into db
 
-// read the appids from file
-const raw_appids = JSON.parse(fs.readFileSync("./initDatabase/assets/steamAppDetails.json"));
-const appids = raw_appids.applist.apps.map(data => data.appid); // extract only the appids
-console.log(appids.length);
-
-// make request to the steam storefront for appdetails
-for (let i = 0; i < SAMPLE_AMT; ++i) {
-  fetch("https://store.steampowered.com/api/appdetails?appids="+appids[i])
-    .then((res) => res.json())
-    .then((res) => {
-      if (res[appids[i]].success === true) {
-
-      }
-    })
-    .catch(() => {/* do nothing */});
-}
-
+// closing the connection
 db.close((err) => {
   if (err) return console.error(err.message);
   console.log("Successfully closed the database connection");
 });
+
+
+function getAppIds() {
+  const rawAppids = JSON.parse(fs.readFileSync(APPIDS_PATH)); // read the appids from file
+  return rawAppids.applist.apps.map(data => data.appid); // return only the appids
+}
+
+async function getAppDetails() {
+  const appids = getAppIds(); // get the appids
+  let appdetails = [];
+
+  // make a fetch request for a sample amount of appids
+  for (let i = 0; i < SAMPLE_AMT; ++i) {
+    // request for the data
+    let res = await fetch(REQ_URL + appids[i]);
+    res = await res.json();
+
+    // check if success
+    res = res[appids[i]];
+    if (res.success === true) {
+      appdetails.push(res.data);
+    }
+  }
+
+  return appdetails;
+} 
