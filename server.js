b096a78f503ed2d9be595d965f8fdfa238ba0af5
Gabriel Chung <gabrielchung9@gmail.com>
2022-03-14 22:21:07 -0400

    added header and styling

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 const app = express();
 
 // setting up the view engine
-app.use("view engine", "pug");
+app.set("view engine", "pug");
 
 // log out the requests
 app.use("/", (req, res, next) => {
@@ -10,6 +10,15 @@ app.use("/", (req, res, next) => {
   next();
 });
 app.use(express.static("public"));
+
+// GET routes
+app.get("/home", sendHomepage);
+
+function sendHomepage(req, res) {
+  res.render("pages/homepage", { user: {loggedin: true, userid: 10} });
+}
+
+
 
 // start the server on port 3000
 app.listen(3000, () => {
