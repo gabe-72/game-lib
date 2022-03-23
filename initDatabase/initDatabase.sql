f9a06d2aa3b2d2a69f53a5537481c416c1279f03
Gabriel Chung <gabrielchung9@gmail.com>
2022-03-23 03:24:21 -0400

    db initialization script created

--- initDatabase/initDatabase.sql
+++ initDatabase/initDatabase.sql
@@ -4,25 +4,59 @@ DROP TABLE IF EXISTS stores;
 DROP TABLE IF EXISTS sold_in;
 DROP TABLE IF EXISTS genre_of;
 DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS friends_with;
+DROP TABLE IF EXISTS owns;
 
 CREATE TABLE games(
-  game_id TEXT PRIMARY KEY NOT NULL,
+  game_id INTEGER PRIMARY KEY,
   name TEXT NOT NULL,
+  description TEXT NOT NULL,
   developer TEXT NOT NULL,
   publisher TEXT NOT NULL
 );
 
-CREATE TABLE genres(
-  genre_id TEXT PRIMARY KEY NOT NULL,
-  genre TEXT NOT NULL, -- genre name
+CREATE TABLE stores(
+  store_id INTEGER PRIMARY KEY,
+  name TEXT NOT NULL, -- store name
+  developer TEXT NOT NULL -- developer name
 );
 
-CREATE TABLE stores(
-  store_id TEXT PRIMARY KEY NOT NULL,
-  name TEXT NOT NULL, -- store name
-  developer TEXT NOT NULL, -- developer name
+CREATE TABLE sold_in(
+  game_id INTEGER,
+  store_id INTEGER,
+  price TEXT NOT NULL,
+  FOREIGN KEY(game_id) REFERENCES games(game_id),
+  FOREIGN KEY(store_id) REFERENCES stores(store_id),
+  PRIMARY KEY(game_id, store_id)
+);
+
+CREATE TABLE genres(
+  genre_id INTEGER PRIMARY KEY,
+  genre TEXT NOT NULL -- genre name
+);
+
+CREATE TABLE genre_of(
+  game_id INTEGER,
+  genre_id INTEGER,
+  FOREIGN KEY(game_id) REFERENCES games(game_id),
+  FOREIGN KEY(genre_id) REFERENCES genres(genre_id),
+  PRIMARY KEY(game_id, genre_id)
+);
+
+CREATE TABLE users(
+  user_id INTEGER PRIMARY KEY,
+  email TEXT UNIQUE NOT NULL,
+  username TEXT NOT NULL,
+  password TEXT NOT NULL
+);
+
+CREATE TABLE owns(
+  user_id INTEGER,
+  game_id INTEGER,
+  FOREIGN KEY(user_id) REFERENCES users(user_id),
+  FOREIGN KEY(game_id) REFERENCES games(game_id),
+  PRIMARY KEY(user_id, game_id)
 );
 
 INSERT INTO stores (name, developer) VALUES ("Steam", "Valve");
-INSERT INTO stores (name, developer) VALUES ("Epic Games Store", "Epic Games")
+INSERT INTO stores (name, developer) VALUES ("Epic Games Store", "Epic Games");
+INSERT INTO stores (name, developer) VALUES ("GOG.com", "CD Projekt");
--- initDatabase/initSteamData.js
+++ initDatabase/initSteamData.js
@@ -2,13 +2,13 @@ import fetch from "node-fetch";
 import * as fs from "fs";
 import sqlite3 from "sqlite3";
 
-const SAMPLE_AMT = 15;
+const SAMPLE_AMT = 30;
 const APPIDS_PATH = "./initDatabase/assets/steamAppDetails.json";
 const REQ_URL = "https://store.steampowered.com/api/appdetails?appids=";
 
 // get the appdetails
 console.log("Getting appdetails...");
-const appdetails = getAppDetails();
+const appdetails = await getAppDetails();
 
 // connect to the database
 let db = new sqlite3.Database("./game_lib.db", (err) => {
@@ -16,9 +16,16 @@ let db = new sqlite3.Database("./game_lib.db", (err) => {
   console.log("Connected to database");
 });
 
-// TODO: insert into db
-for (let i = 0; i < appdetails.length; ++i) {
-}
+// inserting into database
+db.serialize(function() {
+  db.run("DELETE FROM games");
+  let stmtgames = db.prepare("INSERT OR REPLACE INTO games (name, description, developer, publisher) VALUES (?, ?, ?, ?)");
+  for (let i = 0; i < appdetails.length; ++i) {
+    let game = appdetails[i];
+    stmtgames.run([game.name, game.detailed_description, game.developers.join(","), game.publishers.join(",")]);
+  }
+  stmtgames.finalize();
+});
 
 // closing the connection
 db.close((err) => {
